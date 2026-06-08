dc790e3a96ec6fd4b976e710b81f97bd1f05bbce
Yasser Toruno <yasser@yassers-mbp.lan>
2026-06-07 20:46:26 -0400

    use the new base panel admin when dynamically re-generating admins

--- dj_control_room/admin.py
+++ dj_control_room/admin.py
@@ -1,36 +1,21 @@
 from django.contrib import admin
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+
+from dj_control_room_base.core import BasePanelAdmin
 
 from .admin_integration import unregister_panel_placeholders
 from .models import DjControlRoomDashboard
 
 
 @admin.register(DjControlRoomDashboard)
-class DjControlRoomDashboardAdmin(admin.ModelAdmin):
+class DjControlRoomDashboardAdmin(BasePanelAdmin):
     """
     Admin entry for the DJ Control Room dashboard.
-    
-    This shows up first in the DJ Control Room section of the admin sidebar.
-    Individual panels are registered dynamically via admin_integration.py
+
+    Shows up first in the DJ Control Room section of the admin sidebar.
+    Individual panels are registered dynamically via admin_integration.py.
     """
-    def changelist_view(self, request, extra_context=None):
-        # Redirect to the main Control Room dashboard
-        return HttpResponseRedirect(reverse("dj_control_room:index"))
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        # Allow staff members to "view" the dashboard
-        return request.user.is_staff
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_view_permission(self, request, obj=None):
-        # Allow staff members to view the dashboard
-        return request.user.is_staff
+    redirect_url_name = "dj_control_room:index"
 
 
 # Unregister panel placeholders so they only appear under DJ Control Room.
